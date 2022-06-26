fb22c2bed349c699ea569bc4d77b41e6ca983315
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2022-06-26 18:04:03 +0800

    Added Visualize using Prefabs

--- Assets/Scripts/MapData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public struct MapData{
-    public bool[] obstacleArray;
-    public List<KnightPiece> knightPiecesList;
-    public Vector3 startPosition;
-    public Vector3 exitPosition;
-}
